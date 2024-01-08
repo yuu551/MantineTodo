57eec6c837bad04c86b6db687be3dfe553e82b5d
Yudai Jinno <nakou36@gmail.com>
2024-01-08 21:38:47 +0900

    タスク追加ダイアログを修正

--- src/components/TodoAddArea.tsx
+++ src/components/TodoAddArea.tsx
@@ -5,13 +5,54 @@ import {
   Modal,
   TextInput,
   Select,
+  Divider,
+  Group,
+  useCombobox,
+  Combobox,
+  InputBase,
+  Input,
 } from "@mantine/core";
 import { DateTimePicker } from "@mantine/dates";
 import { useDisclosure } from "@mantine/hooks";
 import { IconPlus } from "@tabler/icons-react";
+import { Indicator } from "@mantine/core";
+import { useState } from "react";
+
+type Category = {
+  value: string;
+  color: string;
+};
+
+const Categories: Category[] = [
+  { value: "家事", color: "lime" },
+  { value: "仕事", color: "grape" },
+  { value: "その他", color: "gray" },
+];
+
+const SelectOption = ({ value, color }: Category) => {
+  return (
+    <Group>
+      <Indicator color={color}></Indicator>
+      <Text>{value}</Text>
+    </Group>
+  );
+};
 
 export function TodoAddArea() {
   const [opened, { open, close }] = useDisclosure(false);
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+
+  const [value, setValue] = useState<string | null>(null);
+  const selectedOption = Categories.find((item) => item.value === value);
+
+  const options = Categories.map((item) => (
+    <Combobox.Option value={item.value} key={item.value}>
+      <SelectOption {...item} />
+    </Combobox.Option>
+  ));
+
   return (
     <Container size={"md"} mt={30}>
       <Button
@@ -27,15 +68,67 @@ export function TodoAddArea() {
       <Modal
         opened={opened}
         onClose={close}
-        title={<Text fw={"bold"}>Todo追加</Text>}
-        centered
+        withCloseButton={false}
+        radius={16}
+        size={600}
+        title={<Text fw={"bold"}>タスク追加</Text>}
       >
-        <TextInput label="やること" />
-        <DateTimePicker mt={10} label="期日" valueFormat="YYYY/MM/DD HH:mm" />
-        <Select mt={10} label="カテゴリー" data={["家事", "技術", "その他"]} />
-        <Button fullWidth leftSection={<IconPlus />} my={20}>
-          追加
-        </Button>
+        <TextInput placeholder="タスク名" radius="xl" variant="unstyled" />
+        <DateTimePicker
+          mt={3}
+          placeholder="期限"
+          radius="xl"
+          variant="unstyled"
+          valueFormat="YYYY/MM/DD HH:mm"
+        />
+        <Divider my="xs" />
+        <Group justify="space-between">
+          {/** <Select
+            placeholder="カテゴリ"
+            radius="xl"
+            variant="unstyled"
+            w={300}
+            data={["家事", "仕事", "その他"]}
+          />*/}
+
+          <Combobox
+            store={combobox}
+            onOptionSubmit={(val) => {
+              setValue(val);
+              combobox.closeDropdown();
+            }}
+          >
+            <Combobox.Target>
+              <InputBase
+                component="button"
+                type="button"
+                pointer
+                onClick={() => combobox.toggleDropdown()}
+                rightSectionPointerEvents="none"
+                multiline
+                variant="unstyled"
+                w={300}
+              >
+                {selectedOption ? (
+                  <SelectOption {...selectedOption} />
+                ) : (
+                  <Input.Placeholder>カテゴリ</Input.Placeholder>
+                )}
+              </InputBase>
+            </Combobox.Target>
+
+            <Combobox.Dropdown>
+              <Combobox.Options>{options}</Combobox.Options>
+            </Combobox.Dropdown>
+          </Combobox>
+
+          <Group style={{ flexDirection: "row-reverse" }}>
+            <Button size="xs">タスクを追加</Button>
+            <Button size="xs" color="gray">
+              キャンセル
+            </Button>
+          </Group>
+        </Group>
       </Modal>
     </Container>
   );
